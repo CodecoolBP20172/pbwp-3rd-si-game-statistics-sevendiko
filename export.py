12b8ec18bf4ecaffb77a189a9e4621caab72311e
Koperecz Dániel <sevendiko@gmail.com>
2017-06-26 00:18:59 +0200

    initial commit

--- export.py
+++ export.py
@@ -1,2 +1,10 @@
+from reports import count_games, decide, get_latest, count_by_genre, get_line_number_by_title, sort_abc, get_genres
+from pprint import pprint
 
-# Export functions
+pprint(count_games("/home/sevendiko/Asztal/codecool/pbwp-3rd-si-game-statistics-sevendiko/game_stat.txt"))
+pprint(decide("/home/sevendiko/Asztal/codecool/pbwp-3rd-si-game-statistics-sevendiko/game_stat.txt", '1976'))
+pprint(get_latest("/home/sevendiko/Asztal/codecool/pbwp-3rd-si-game-statistics-sevendiko/game_stat.txt"))
+pprint(count_by_genre("/home/sevendiko/Asztal/codecool/pbwp-3rd-si-game-statistics-sevendiko/game_stat.txt", 'Survival game'))
+pprint(get_line_number_by_title("/home/sevendiko/Asztal/codecool/pbwp-3rd-si-game-statistics-sevendiko/game_stat.txt", 'Diablo III'))
+pprint(get_genres("/home/sevendiko/Asztal/codecool/pbwp-3rd-si-game-statistics-sevendiko/game_stat.txt"))
+pprint(sort_abc("/home/sevendiko/Asztal/codecool/pbwp-3rd-si-game-statistics-sevendiko/game_stat.txt"))
--- reports.py
+++ reports.py
@@ -1,2 +1,58 @@
+def count_games(filename):
+    with open(filename) as f:
+        games = sum(1 for _ in f)
+        return games
 
-# Report functions
+
+def decide(filename, year):
+    with open(filename) as f:
+        games = f.readlines()
+        dates = [date.split('\t')[2] for date in games]
+        for year in dates:
+            if year not in dates:
+                return False
+            else:
+                return True
+
+
+def get_latest(filename):
+    with open(filename) as f:
+        games = f.readlines()
+        latest = max([year.split('\t')[2] for year in games])
+        for item in games:
+            if latest in item:
+                game = str(item.split('\t')[0])
+        return game
+
+
+def count_by_genre(filename, genre):
+    with open(filename) as f:
+        games = f.readlines()
+        genres = [genres.split('\t')[3] for genres in games]
+        if genre in genres:
+            counted_by_genre = genres.count(genre)
+            return counted_by_genre
+
+
+def get_line_number_by_title(filename, title):
+    with open(filename) as f:
+        games = f.readlines()
+        titles = [title.split('\t')[0] for title in games]
+        if title in titles:
+            line_number_of_title = titles.index(title) + 1
+            return line_number_of_title
+
+
+def sort_abc(filename):
+    with open(filename) as f:
+        games = f.readlines()
+        titles = sorted(title.split('\t')[0] for title in games)
+        return titles
+
+
+def get_genres(filename):
+    with open(filename) as f:
+        games = f.readlines()
+        genres = {genres.split('\t')[3] for genres in games}
+        return sorted(genres, key=str.swapcase)
+
